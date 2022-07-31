8a86a7fd5d6c43e2ee91c1d364f3dad0ee1bbd4e
pangli <pangli@58.com>
2022-07-31 22:51:15 +0800

    提交第8章部分Demo内容

--- DemoApp/app/src/main/java/com/guo/awesome/comopse/chapter/eight/WorkWithJetpacks.kt
+++ DemoApp/app/src/main/java/com/guo/awesome/comopse/chapter/eight/WorkWithJetpacks.kt
@@ -1,25 +1,32 @@
 package com.guo.awesome.comopse.chapter.eight
 
 import android.content.Context
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.edit
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.paging.LoadState
+import androidx.paging.PagingData
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.items
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class MyViewModel(defaultVal: Int) : ViewModel() {
     private val _count = MutableLiveData(defaultVal)
@@ -57,6 +64,57 @@ fun MyScreen() {
             sp.edit { putInt(key, newCount) }
         }) {
             Text(text = "Increment")
+        }
+    }
+}
+
+
+fun testFlow(): Flow<Int> = flow {
+    for (i in 0..10) {
+        kotlinx.coroutines.delay(1000)
+        emit(i)
+    }
+}
+
+@Composable
+fun TestFlow() {
+    val flowValue = testFlow().collectAsState(initial = 0)
+    Text(
+        text = flowValue.value.toString(),
+        modifier = Modifier.fillMaxWidth(),
+        fontSize = 32.sp,
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun TestRxJava() {
+    //Observable<Int>.subscribeAsState(initial = 0)
+}
+
+
+@Composable
+fun TestPaging(flow: Flow<PagingData<String>>) {
+    val lazyPagingItems = flow.collectAsLazyPagingItems()
+    LazyColumn {
+        items(lazyPagingItems) {
+            Text("Item is $it")
+        }
+
+        val state = lazyPagingItems.loadState
+        when {
+            state.prepend is LoadState.Loading -> {
+
+            }
+            state.prepend is LoadState.Error -> {
+
+            }
+            state.prepend is LoadState.NotLoading -> {
+
+            }
+            state.append is LoadState.Loading -> {
+
+            }
         }
     }
 }
---
+++ DemoApp/app/src/main/java/com/guo/awesome/comopse/chapter/five/Beans.kt
@@ -0,0 +1,9 @@
+package com.guo.awesome.comopse.chapter.five
+
+class Message {
+    var value: String = ""
+}
+
+class Photo {
+
+}
